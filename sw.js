adb917e5df84cee0a6e5022e0659193ee50d8b54
Allanshair <allancarlos.belezacapilar@gmail.com>
2026-08-27 21:40:04 -0300

    Add icon-192.png to service worker cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'agenda-allan-v1';
-const ASSETS = ['./index.html', './manifest.json', './icon.svg'];
+const ASSETS =['./index.html', './manifest.json', './icon.svg', './icon-192.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
