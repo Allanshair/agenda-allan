7868dd14c2827d847df71fd4f1d35ebb76d7b1f2
Allanshair <allancarlos.belezacapilar@gmail.com>
2026-08-29 12:40:10 -0300

    Refactor service worker for improved caching
    Updated service worker to version 6 with new caching strategies and additional assets.

--- sw.js
+++ sw.js
@@ -1,8 +1,73 @@
-const CACHE = 'agenda-allan-v5';
-const ASSETS =['./index.html', './manifest.json', './icon.svg', './icon-192.png'];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+const CACHE = "agenda-allan-v6";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+
+  self.skipWaiting();
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+
+  if (request.method !== "GET") {
+    return;
+  }
+
+  const url = new URL(request.url);
+
+  if (
+    url.pathname.endsWith("/manifest.json") ||
+    url.pathname.endsWith("/icon-192.png") ||
+    url.pathname.endsWith("/icon-512.png") ||
+    url.pathname.endsWith("/icon.svg")
+  ) {
+    event.respondWith(
+      fetch(request, { cache: "no-store" }).then((response) => {
+        const responseCopy = response.clone();
+
+        caches.open(CACHE).then((cache) => {
+          cache.put(request, responseCopy);
+        });
+
+        return response;
+      }).catch(() => {
+        return caches.match(request);
+      })
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cachedResponse) => {
+      return cachedResponse || fetch(request);
+    })
+  );
 });
